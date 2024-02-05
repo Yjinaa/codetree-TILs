85232928831e49a4eb41b98100e1c3bf7116b843
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-02-05 22:35:46 +0900

    Title: 배열 채우기, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240205/배열 채우기/filling-array.py
+++ 240205/배열 채우기/filling-array.py
@@ -4,6 +4,8 @@ for elem in case:
     if elem != 0:
         arr.append(elem)
     else:
-        arr = arr[::-1]
-        for elem in arr:
-            print(elem, end=" ")
+        break
+
+arr = arr[::-1]
+for elem in arr:
+    print(elem, end=" ")
