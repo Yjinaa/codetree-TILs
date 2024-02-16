1538f8c98802eec8f4af27c784cb4b89b2976d63
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-02-16 22:02:11 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240216/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -0,0 +1,15 @@
+import sys
+n = int(input())
+ns = list(map(int, input().split()))
+maxes = []
+maxv = -sys.maxsize
+while maxv != ns[0]:
+    maxv = -sys.maxsize
+    for elem in ns:
+        if elem > maxv:
+            maxv = elem
+    maxes.append(ns.index(maxv)+1)
+    if len(ns) <= 2:
+        print(*maxes)
+    else:
+        ns = ns[:ns.index(maxv)]
