eb4456172841cd18777de21991d639b98cdba800
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-02-16 23:47:02 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 240216/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 240216/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -1,15 +1,13 @@
 import sys
 n = int(input())
 ns = list(map(int, input().split()))
-maxes = []
-maxv = -sys.maxsize
-while maxv != ns[0]:
-    maxv = -sys.maxsize
+
+while True:
+    maxv = ns[0]
     for elem in ns:
         if elem > maxv:
             maxv = elem
-    maxes.append(ns.index(maxv)+1)
-    if len(ns[:ns.index(maxv)]) <= 1:
-        print(*maxes)
-    else:
-        ns = ns[:ns.index(maxv)]
+    print(ns.index(maxv)+1, end=" ")
+    if ns.index(maxv) == 0:
+        break
+    ns = ns[:ns.index(maxv)]
