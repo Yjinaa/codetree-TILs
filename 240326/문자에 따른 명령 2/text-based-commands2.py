3066d8a3c399486dec93efa3a66d51ce36eef9a3
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-03-26 21:06:33 +0900

    Title: 문자에 따른 명령 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240326/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,18 @@
+directions = list(input())
+
+x, y = 0,0
+
+dx = [1,0,-1,0]
+dy = [0,-1,0,1]
+
+dir_num = 3
+
+for direction in directions:
+    if direction == 'L':
+        dir_num = (dir_num+3)%4 # 0일 때 3이 되어야 함
+    elif direction == 'R':
+        dir_num = (dir_num+1)%4 # 3일 때 0이 되어야 함
+    else:
+        nx, ny = x + dx[dir_num], y + dy[dir_num]
+
+print(nx, ny)
