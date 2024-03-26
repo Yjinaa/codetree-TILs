5d5f06fcfeecf762923570ba5874c36ea63f8ea6
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-03-26 22:07:24 +0900

    Title: 작은 구슬의 이동, Time: 158ms, Memory: 24MB, Status: Passed - CodeTree

--- 240326/작은 구슬의 이동/small-marble-movement.py
+++ 240326/작은 구슬의 이동/small-marble-movement.py
@@ -11,7 +11,7 @@ dir_map = {"U":2, "D":1, "R":0,"L":3}
 dir_num = dir_map[d]
 
 def in_range(x,y):
-    return 1<=x and x<n and 1<=y and y<n
+    return 1<=x and x<=n and 1<=y and y<=n
 
 for time in range(t):
     nx,ny = x+dx[dir_num], y+dy[dir_num]
