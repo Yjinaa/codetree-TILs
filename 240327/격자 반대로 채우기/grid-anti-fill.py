a0a4b06aa97bf8dd6c51789d4e237279e6144a41
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-03-27 18:38:37 +0900

    Title: 격자 반대로 채우기, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

--- 240327/격자 반대로 채우기/grid-anti-fill.py
+++ 240327/격자 반대로 채우기/grid-anti-fill.py
@@ -4,28 +4,44 @@ arr = [[0 for _ in range(n)] for _ in range(n)]
 
 num = 1
 
-# n-1이 짝수면 짝수번째 줄마다 거꾸로, 홀수면 홀수번째 줄마다 거꾸로
+# # n-1이 짝수면 짝수번째 줄마다 거꾸로, 홀수면 홀수번째 줄마다 거꾸로
 
-if n-1 % 2 == 0:
-    for i in range(n-1,-1,-1):
-        if i % 2 == 0:
-            for j in range(n-1, -1, -1):
-                arr[j][i] = num
-                num += 1
-        else:
-            for j in range(n):
-                arr[j][i] = num
-                num += 1
-else:
-    for i in range(n-1,-1,-1):
-        if i % 2 == 1:
-            for j in range(n-1,-1,-1):
-                arr[j][i] = num
-                num += 1
-        else:
-            for j in range(n):
-                arr[j][i] = num
-                num += 1
+# if n-1 % 2 == 0:
+#     for i in range(n-1,-1,-1):
+#         if i % 2 == 0:
+#             for j in range(n-1, -1, -1):
+#                 arr[j][i] = num
+#                 num += 1
+#         else:
+#             for j in range(n):
+#                 arr[j][i] = num
+#                 num += 1
+# else:
+#     for i in range(n-1,-1,-1):
+#         if i % 2 == 1:
+#             for j in range(n-1,-1,-1):
+#                 arr[j][i] = num
+#                 num += 1
+#         else:
+#             for j in range(n):
+#                 arr[j][i] = num
+#                 num += 1
+
+direction = 'reverse'
+for i in range(n-1,-1,-1):
+    if direction == 'reverse':
+        for j in range(n-1,-1,-1):
+            arr[j][i] = num
+            num += 1
+        direction = 'normal'
+    else:
+        for j in range(n):
+            arr[j][i] = num
+            num += 1
+        direction = 'reverse'
+
+
+        
 
 
 for _arr in arr:
