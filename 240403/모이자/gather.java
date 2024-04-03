3396481cf048c1b5825f68383e24caeb6c1374bf
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-04-03 23:37:39 +0900

    Title: 모이자, Time: 156ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240403/모이자/gather.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static final int MAX_N = 100;
+
+    public static int n;
+    public static int[] a = new int[MAX_N];
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        for(int i=0;i<n;i++){
+            a[i] = sc.nextInt();
+        }
+        int ans = INT_MAX;
+        for(int i=0;i<5;i++){
+            int distance=0;
+            for(int j=0;j<5;j++){
+                distance += a[j] * Math.abs(j-i);
+            }
+            if(distance<ans){
+                ans = distance;
+            }
+        }
+        System.out.print(ans);
+
+    }
+}
