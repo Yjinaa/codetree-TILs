383b7f4872dc92dc6646b16d86e5e070e2bb43dc
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-07-03 22:42:38 +0900

    Title: Date to Date, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240703/Date to Date/date-to-date.py
@@ -0,0 +1,20 @@
+times = list(map(int, input().split()))
+
+m1, d1, m2, d2 = times
+
+
+days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+start = 0
+end = 0
+
+for i in range(m1):
+    start += days[i]
+start += d1
+
+for i in range(m2):
+    end += days[i]
+end += d2
+
+
+print(end - start -1)
