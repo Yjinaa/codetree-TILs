2d8710d910cbb38f14dde42a541469d087ea798a
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-03 23:34:58 +0900

    Title: 되돌아오기, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240903/되돌아오기/come-back.py
+++ 240903/되돌아오기/come-back.py
@@ -1,3 +1,21 @@
+def find_answer(n):
+    x, y = 0,0
+    t = 0
+
+    for i in range(n):
+        direction, distance = input().split()
+        d = dir_map[direction]
+        distance = int(distance)
+
+        for j in range(distance):
+            nx, ny = x + dx[d], y + dy[d]
+            x, y = nx, ny
+            t += 1
+
+            if nx == 0 and ny == 0:
+                return (t)
+    return -1
+
 n = int(input())
 
 dx = [1, -1, 0, 0]
@@ -5,21 +23,4 @@ dy = [0, 0, -1, 1]
 
 dir_map = {'E':0, 'W':1, 'S':2, 'N':3}
 
-x, y = 0,0
-t = 0
-
-for i in range(n):
-    direction, distance = input().split()
-    d = dir_map[direction]
-    distance = int(distance)
-
-    for j in range(distance):
-        nx, ny = x + dx[d], y + dy[d]
-        x, y = nx, ny
-        t += 1
-
-        if nx == 0 and ny == 0:
-            print(t)
-            exit()
-
-print(-1)
+print(find_answer(n))
