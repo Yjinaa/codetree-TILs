0115558a7c15a8613a4b6ea9a4b7da36957e51ae
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-05 01:19:22 +0900

    Title: 사는 지역, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240905/사는 지역/where-live.py
@@ -0,0 +1,22 @@
+class Person:
+    def __init__(self, name, code, location):
+        self.name = name
+        self.code = code
+        self.location = location
+
+n = int(input())
+
+people = []
+
+for i in range(n):
+    name, code, location = input().split()
+    people.append(Person(name, code, location))
+
+target_idx = 0
+for i, person in enumerate(people):
+    if person.name > people[target_idx].name:
+        target_idx = i 
+
+print(f'name {people[taget_idx].name}')
+print(f'name {people[taget_idx].code}')
+print(f'name {people[taget_idx].location}')
