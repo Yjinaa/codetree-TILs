56dde4121af9bf39366c8950de3669569c830c1c
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-07 01:16:53 +0900

    Title: 키를 기준으로 정렬, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240907/키를 기준으로 정렬/sort-by-height.py
@@ -0,0 +1,18 @@
+class User:
+    def __init__(self,name,height,weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+
+
+n = int(input())
+
+users = []
+for i in range(n):
+    n, h, w = input().split()
+    users.append(User(n, h, w))
+
+users.sort(key=lambda x:x.height)
+
+for i in range(5):
+    print(f'{users[i].name} {users[i].height} {users[i].weight}')
