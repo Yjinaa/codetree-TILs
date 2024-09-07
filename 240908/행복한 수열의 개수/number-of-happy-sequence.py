f89af583738db1a7f7e5648eeb75b3724b090761
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-08 02:48:02 +0900

    Title: 행복한 수열의 개수, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240908/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240908/행복한 수열의 개수/number-of-happy-sequence.py
@@ -5,7 +5,7 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 seq = [0 for _ in range(n)]
 
 def is_happy_sequence(seq):
-    consecutive_count = 1, max_cnt = 1
+    consecutive_count, max_cnt = 1, 1
     for i in range(1, n):
         if seq[i-1] == seq[i]:
             consecutive_count += 1
