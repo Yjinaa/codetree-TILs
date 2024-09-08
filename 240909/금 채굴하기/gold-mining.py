420f23409499474ba1173486c4e24c6d435b69f0
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-09 03:17:14 +0900

    Title: 금 채굴하기, Time: 458ms, Memory: 26MB, Status: Passed - CodeTree

--- 240909/금 채굴하기/gold-mining.py
+++ 240909/금 채굴하기/gold-mining.py
@@ -1,50 +1,25 @@
 n, m = map(int, input().split())
 
 grid = [list(map(int, input().split())) for _ in range(n)]
-n_grid = [[0]*(len(grid[0])+2)]
-for row in grid:
-      n_grid.append([0] + row + [0])
-n_grid.append([[0]*(len(grid[0])+2)])
 
 def get_expenditure(k):
     return k*k + (k+1)*(k+1)
 
-def get_k(k):
-      size = 2*k+1
-      k_grid = [[0]*(size) for _ in range(size)]
-      center = k
+def get_num_of_gold(row, col, k):
+      cnt_gold = 0
+      for i in range(n):
+            for j in range(n):
+                  if abs(row-i) + abs(col-j) <= k and grid[i][j] == 1:
+                        cnt_gold += 1
+      return cnt_gold
 
-      for i in range(size):
-            for j in range(size):
-                  if abs(center-i) + abs(center-j) <= k:
-                        k_grid[i][j] = 1
-      return k_grid
-
-def get_gold(x,y,k):
-      gold_cnt = 0
-      for dx in range(len(k)):
-            for dy in range(len(k)):
-                  if k[dx][dy] == 0:
-                        continue
-                  if 0 <= x + dx < n+1 and 0 <= y + dy < n+1 and k[dx][dy] == 1:
-                        if 0 <= x + dx < n and 0 <= y + dy < n and grid[x+dx][y+dy] == 1:
-                              gold_cnt += 1
-                  else:
-                        break
-      return gold_cnt
-
-import sys
-max_benefit = -sys.maxsize
-max_gold_cnt = 0
-
-for k in range((n+1)//2+1):
+max_gold = 0
+for k in range(2*(n-1)+1):
       for row in range(n):
             for col in range(n):
-                  gold_cnt = get_gold(row, col, get_k(k))
+                  cnt_gold = get_num_of_gold(row, col, k)
                   cost = get_expenditure(k)
-                  final_benefit = gold_cnt*m - cost
-                  if final_benefit > 0:
-                        max_gold_cnt = max(gold_cnt, max_gold_cnt)
-                  
+                  if m * cnt_gold >= cost:
+                        max_gold = max(cnt_gold, max_gold)
 
-print(max_gold_cnt)
+print(max_gold)
