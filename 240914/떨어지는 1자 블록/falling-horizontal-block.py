4fd383bc6ba34e7826e0188c80948065c9d66a5f
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-14 00:47:54 +0900

    Title: 떨어지는 1자 블록, Time: 5061ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

---
+++ 240914/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,20 @@
+n, m, k = map(int, input().split())
+k = k-1
+
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+def simulate():
+    while True:
+        is_touched = False
+        for row in range(n):
+            for col in range(k, k+m):
+                # print(f'grid[{row}][{col}] = {grid[row][col]}')
+                if grid[row][col] == 0:
+                    continue
+                else:
+                    grid[row-1][k:k+m] = [1] * m
+                    return grid
+                
+grid = simulate()
+for row in grid:
+    print(*row)
