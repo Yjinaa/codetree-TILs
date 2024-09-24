459f930facb8d5074ffbf925a6e2d0d67b8851f5
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-24 15:30:53 +0900

    Title: 두 수의 합, Time: 219ms, Memory: 36MB, Status: Passed - CodeTree

--- 240924/두 수의 합/sum-of-two-num.py
+++ 240924/두 수의 합/sum-of-two-num.py
@@ -1,14 +1,14 @@
 from collections import defaultdict
+from collections import Counter
 n, k = map(int, input().split())
 
 cnt = 0
 nums = list(map(int, input().split()))
-complements = defaultdict(int)
-for num in nums:
+complements = defaultdict(list)
+for i,num in enumerate(nums):
     complement = k-num
     if complement in complements:
-        cnt += 1
-    else:
-        complements[num] = complement
+        cnt += len(complements[complement])
+    complements[num].append(i)
 
 print(cnt)
