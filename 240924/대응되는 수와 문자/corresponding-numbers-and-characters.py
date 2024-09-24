3ddb155f47434dc5c8d1dad16039059645b2ce36
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-24 15:06:04 +0900

    Title: 대응되는 수와 문자, Time: 820ms, Memory: 49MB, Status: Passed - CodeTree

--- 240924/대응되는 수와 문자/corresponding-numbers-and-characters.py
+++ 240924/대응되는 수와 문자/corresponding-numbers-and-characters.py
@@ -4,12 +4,14 @@ d = defaultdict(int)
 for i in range(n):
     elem = input()
     d[elem] = i+1
+    d[i+1] = elem
 
 for i in range(m):
     target = input()
     if target.isnumeric():
-        for key in d:
-            if d[key] == int(target):
-                print(key)
+        print(d[int(target)])
+        # for key in d:
+        #     if d[key] == int(target):
+        #         print(key)
     else:
         print(d[target])
