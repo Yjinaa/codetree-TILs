086c371c54cabaade67d2b32c556f40b7a48bfe9
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-24 22:21:56 +0900

    Title: 세 수의 합, Time: 194ms, Memory: 25MB, Status: Passed - CodeTree

--- 240924/세 수의 합/sum-of-three-num.py
+++ 240924/세 수의 합/sum-of-three-num.py
@@ -1,20 +1,19 @@
-# 하나의 수를 정하고, 그거 뺀 나머지 수에서 two sum
+# 하나의 수를 정하고, 그거 뺀 나머지 수에서 반복
 
-from collections import defaultdict
+from collections import Counter
 
 n,k = map(int, input().split())
 
-nums = sorted(list(map(int, input().split())))
-
-# -1, 1, 1, 2, 4
+nums = list(map(int, input().split()))
+complements = Counter(nums)
 
 cnt = 0
 
 for i in range(n):
-    for left in range(i + 1, n):  # i 다음 인덱스부터 시작
-        for right in range(left + 1, n):  # left 다음 인덱스부터 시작
-            target = nums[i] + nums[left] + nums[right]
-            if target == k:
-                cnt += 1
+    complements[nums[i]] -= 1
 
+    for j in range(i):
+        diff = k - nums[i] - nums[j]
+        if diff in complements:
+            cnt += complements[diff]
 print(cnt)
