6e89b8058106caadcfd5ca103fdaeadd4ab7ab89
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-27 21:31:36 +0900

    Title: 그래프 탐색, Time: 148ms, Memory: 26MB, Status: Passed - CodeTree

--- 240927/그래프 탐색/graph-traversal.py
+++ 240927/그래프 탐색/graph-traversal.py
@@ -1,12 +1,21 @@
 n, m = map(int, input().split())
 
 graph = [[] for _ in range(n+1)]
-visited = [False for _ in range(n+1)]
 
 for _ in range(m):
     start, end = map(int, input().split())
     graph[start].append(end)
     graph[end].append(start)
 
+visited = [False for _ in range(n+1)]
+ans = 0
 
-print(len(graph[1]))
+def dfs(node):
+    visited[node] = True
+    # ans += 1
+    for neighbor in graph[node]:
+        if not visited[neighbor]:
+            dfs(neighbor)
+
+dfs(1)
+print(len([x for x in visited if x != False])-1)
