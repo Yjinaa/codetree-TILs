260dddfeeb08315a7121179c657dafd0353924cb
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-28 02:41:37 +0900

    Title: 갈 수 있는 곳들, Time: 135ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240928/갈 수 있는 곳들/places-can-go.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+n, k = map(int, input().split())
+
+grid = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False]*n for _ in range(n)]
+
+dxs = [0, 1, 0, -1]
+dys = [1, 0, -1, 0]
+
+def bfs():
+    available_paths = 1
+    while queue:
+        cx, cy = queue.popleft()
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] != True and grid[nx][ny] == 0:
+                queue.append((nx,ny))
+                visited[nx][ny] = True
+                available_paths += 1
+    return available_paths
+
+total_paths = 0
+for _ in range(k):
+    x, y = map(int, input().split())
+    x, y = x-1, y-1
+    queue = deque([(x,y)])
+    available_paths = bfs()
+    total_paths += available_paths
+
+print(total_paths)
