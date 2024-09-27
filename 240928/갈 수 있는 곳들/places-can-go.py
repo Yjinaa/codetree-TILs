38b4e7b2db2492bbb84a6ad05bbb2839de2a6519
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-28 02:53:33 +0900

    Title: 갈 수 있는 곳들, Time: 242ms, Memory: 26MB, Status: Passed - CodeTree

--- 240928/갈 수 있는 곳들/places-can-go.py
+++ 240928/갈 수 있는 곳들/places-can-go.py
@@ -4,6 +4,7 @@ n, k = map(int, input().split())
 
 grid = [list(map(int, input().split())) for _ in range(n)]
 visited = [[False]*n for _ in range(n)]
+check_board = [[0]*n for _ in range(n)]
 
 dxs = [0, 1, 0, -1]
 dys = [1, 0, -1, 0]
@@ -13,10 +14,11 @@ def bfs():
     while queue:
         cx, cy = queue.popleft()
         for dx, dy in zip(dxs, dys):
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] != True and grid[nx][ny] == 0:
+            nx, ny = cx + dx, cy + dy
+            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] != True and grid[nx][ny] != 1:
                 queue.append((nx,ny))
                 visited[nx][ny] = True
+                check_board[nx][ny] = 1
                 available_paths += 1
     return available_paths
 
@@ -24,8 +26,9 @@ total_paths = 0
 for _ in range(k):
     x, y = map(int, input().split())
     x, y = x-1, y-1
+    check_board[x][y] = 1
     queue = deque([(x,y)])
     available_paths = bfs()
     total_paths += available_paths
 
-print(total_paths)
+print(sum(sum(row) for row in check_board))
