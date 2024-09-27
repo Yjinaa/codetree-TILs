f29a74536d538b8c9d802aef677ddc3118dc4499
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-27 17:09:28 +0900

    Title: 초대장과 번호표, Time: 2491ms, Memory: 35MB, Status: Passed - CodeTree

--- 240927/초대장과 번호표/invitation-and-number-tag.py
+++ 240927/초대장과 번호표/invitation-and-number-tag.py
@@ -7,11 +7,9 @@ invited = {1}
 while True:
     is_changed = False
     for group in groups:
-        if len(group) - len(invited) == 1:
-            remaining_list = [x for x in group if x not in invited]
-            if len(remaining_list) == 1:
-                invited.update(remaining_list)
-                is_changed = True
+        if len(set(group) - invited) == 1:
+            invited.update(set(group)-invited)
+            is_changed = True
     if is_changed == False:
         break
 
