324412760236b29bc052bd4ab84ea4d9a44b43ed
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-27 17:15:58 +0900

    Title: 초대장과 번호표, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240927/초대장과 번호표/invitation-and-number-tag.py
+++ 240927/초대장과 번호표/invitation-and-number-tag.py
@@ -1,16 +1,35 @@
+from collections import deque
 n, g = map(int, input().split())
 
-groups = sorted([list(map(int, input().split())) for _ in range(g)], key=lambda x:x[0])
-groups = [group[1:] for group in groups]
-invited = {1}
+invited = [False] * n
 
-while True:
-    is_changed = False
-    for group in groups:
-        if len(set(group) - invited) == 1:
-            invited.update(set(group)-invited)
-            is_changed = True
-    if is_changed == False:
-        break
+groups = [set() for _ in range(g)]
 
-print(len(invited))
+people_groups = [[] for _ in range(n)]
+
+queue = deque()
+ans = 0
+
+for i in range(g):
+    nums = list(map(int, input().split()))[1:]
+    for nun in nums:
+        num -= 1
+        groups[i].add(num)
+        people_groups[num].append(i) # 각 사람이 어떤 그룹에 들어가있는지
+
+q.append(0)
+invited[0] = True
+
+while queue:
+    x = queue.popleft()
+    ans += 1
+
+    for g_num in people_groups[x]:
+        groups[g_num].remove(x)
+        if len(groups[g_num]) == 1:
+            p_num = list(groups[g_num])[0]
+            if not invited[p_num]:
+                invited[p_num] = True
+                queue.append(p_num)
+
+print(ans)
