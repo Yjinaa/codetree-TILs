7c718c3d006962b05679456554341d4f9b585c05
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-27 22:09:37 +0900

    Title: 마을 구분하기, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240927/마을 구분하기/seperate-village.py
@@ -0,0 +1,31 @@
+n = int(input())
+
+grid = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
+dxs = [0, 1, 0 -1]
+dys = [1, 0, -1, 0]
+
+def dfs(x,y):
+    visited[x][y] = True
+    people = 1
+
+    for dx, dy in zip(dxs, dys):
+        nx = x + dx
+        ny = y + dy
+        
+        if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] != True and grid[nx][ny] != 0:
+            visited[nx][ny]
+            people += dfs(nx,ny)
+    return people
+
+vilage = 0
+total_people = []
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j] and grid[i][j] == 1:
+            people = dfs(i,j)
+            vilage += 1
+            total_people.append(people)
+print(vilage)
+for people in sorted(total_people):
+    print(people)
