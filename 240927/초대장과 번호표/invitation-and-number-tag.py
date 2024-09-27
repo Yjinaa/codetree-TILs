3a2ad8362b47c7bfc2102f35a58baea57575e7a1
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-27 16:59:26 +0900

    Title: 초대장과 번호표, Time: 157ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240927/초대장과 번호표/invitation-and-number-tag.py
+++ 240927/초대장과 번호표/invitation-and-number-tag.py
@@ -4,10 +4,16 @@ groups = sorted([list(map(int, input().split())) for _ in range(g)], key=lambda 
 
 invited = {1}
 
-for group in groups:
-    group.pop(0)
-    remaining_list = [x for x in group if x not in invited]
-    if len(remaining_list) == 1:
-        invited.update(remaining_list)
+while True:
+    is_changed = False
+    for group in groups:
+        if len(group) != 0:
+            group.pop(0)
+            remaining_list = [x for x in group if x not in invited]
+            if len(remaining_list) == 1:
+                invited.update(remaining_list)
+                is_changed = True
+    if is_changed == False:
+        break
 
 print(len(invited))
