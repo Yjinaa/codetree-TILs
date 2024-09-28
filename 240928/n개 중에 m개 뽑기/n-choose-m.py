72a0da9fb266d8e9488349988a0dfdce3addab17
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-28 22:13:51 +0900

    Title: n개 중에 m개 뽑기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240928/n개 중에 m개 뽑기/n-choose-m.py
+++ 240928/n개 중에 m개 뽑기/n-choose-m.py
@@ -4,4 +4,7 @@ num_list = [n for n in range(n+1) if n != 0]
 combs = (list(combinations(num_list, m)))
 
 for com in combs:
-    print(com[0], com[1])
+    if len(num_list) == 1:
+        print(1)
+    else:
+        print(com[0], com[1])
