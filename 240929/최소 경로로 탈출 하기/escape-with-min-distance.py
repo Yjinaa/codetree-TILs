866a9908a56fc4d5fdd1f5a3f9b512e024cbdd7e
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-29 00:56:22 +0900

    Title: 최소 경로로 탈출 하기, Time: 144ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240929/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -0,0 +1,23 @@
+from collections import deque
+n, m = map(int, input().split())
+
+grid = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False]*m for _ in range(n)]
+
+dxs = [0, 1, 0, -1]
+dys = [1, 0, -1, 0]
+
+def bfs():
+    while queue:
+        x, y = queue.popleft()
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] != True and grid[nx][ny] == 1:
+                queue.append((nx, ny))
+                visited[nx][ny] = True
+                grid[nx][ny] = grid[x][y] + 1
+
+grid[0][0] = 0
+queue = deque([(0,0)])
+bfs()
+print(grid[n-1][m-1])
