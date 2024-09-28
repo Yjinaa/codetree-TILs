3ce7aefc367927174a5f4c6262f1594cd12dfd1d
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-09-29 01:13:25 +0900

    Title: 나이트, Time: 129ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240929/나이트/knight-movements.py
@@ -0,0 +1,28 @@
+from collections import deque
+n = int(input())
+
+grid = [[0]*n for _ in range(n)]
+visited = [[False]*n for _ in range(n)]
+
+dxs = [-1,-2,-2,-1,1,2,2,1]
+dys = [-2,-1,1,2,2,1,-1,-2]
+
+def bfs():
+    while queue:
+        x, y = queue.popleft()
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] != True:
+                queue.append((nx,ny))
+                grid[nx][ny] = grid[x][y] + 1
+                visited[nx][ny] = True
+                if nx == r2 and ny == c2:
+                    return
+
+r1, c1, r2, c2 = map(int, input().split())
+r1, c1, r2, c2  = r1-1, c1-1, r2-1, c2-1
+
+
+queue = deque([(r1,c1)])
+bfs()
+print(grid[r2][c2])
