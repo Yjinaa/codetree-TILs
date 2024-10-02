92091868964b9dceeac7a9c5b0d3e63ff6784698
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-10-03 03:14:44 +0900

    Title: 오목, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241003/오목/O-mok.py
+++ 241003/오목/O-mok.py
@@ -27,5 +27,7 @@ for i in range(19):
                             found = True
                             print(grid[i][j])
                             print(((i+x) // 2 + 1), (j+y) // 2 + 1)
-                            # print(x+1,y-2+1)
                             break
+    if found == False:
+        print(0)
+        break
