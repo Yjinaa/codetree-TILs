784120be0b2ddc8cd551bc3977beeb28e8408fad
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-10-10 14:11:28 +0900

    Title: 쪼개어 배낭 채우기 구현, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241010/쪼개어 배낭 채우기 구현/implement-fractional-knapsack.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+
+js = [list(map(int, input().split())) for _ in range(n)]
+js = sorted([[w,v,round(v/w,3)] for w,v in js], key=lambda x:x[2], reverse=True)
+
+v = 0
+for j in js:
+    if m >= j[0]:
+        m -= j[0]
+        v += j[0] * j[2]
+    else:
+        v += m * j[2]
+        m = 0
+    if m == 0:
+        break
+
+print(f'{v:.3f}')
