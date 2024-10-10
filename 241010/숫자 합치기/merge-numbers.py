6c43a244299d4991d614a395e4b080332bd3ea32
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-10-10 14:31:57 +0900

    Title: 숫자 합치기, Time: 5057ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

--- 241010/숫자 합치기/merge-numbers.py
+++ 241010/숫자 합치기/merge-numbers.py
@@ -1,13 +1,16 @@
+import heapq
 n = int(input())
 ns = sorted(list(map(int, input().split())))
+heapq.heapify(ns)
 
 tv = 0
 cur_sum = []
 
 while len(ns) >= 2:
-    min1, min2 = ns.pop(ns.index(min(ns))), ns.pop(ns.index(min(ns)))
+    min1, min2 = heapq.heappop(ns), heapq.heappop(ns)
     v = min1 + min2
     ns.append(v)
+    heapq.heapify(ns)
     tv += v
 
 print(tv)
