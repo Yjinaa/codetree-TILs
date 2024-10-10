43dd55efa0947514ec63438babd06904cfc5be44
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-10-10 16:50:42 +0900

    Title: 초대장과 번호표, Time: 2406ms, Memory: 34MB, Status: Passed - CodeTree

--- 241010/초대장과 번호표/invitation-and-number-tag.py
+++ 241010/초대장과 번호표/invitation-and-number-tag.py
@@ -1,10 +1,20 @@
 n, g = map(int, input().split())
 
 invited = set({1})
+invitation = []
 
 for i in range(g):
-    p = set((list(map(int, input().split())))[1:])
-    if len(p-invited) == 1:
-        invited.update(p-invited)
+    p = list(map(int, input().split()))[1:]
+    invitation.append(p)
+
+while True:
+    is_updated = False
+    for p in invitation:
+        p = set(p)
+        if len(p-invited) == 1:
+            invited.update(p-invited)
+            is_updated = True
+    if is_updated == False:
+        break
 
 print(len(invited))
