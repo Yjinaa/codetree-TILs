73f73b515592a913a77f5d4113f5fe07a3af3bc3
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-10-10 17:12:00 +0900

    Title: 초대장과 번호표, Time: 314ms, Memory: 40MB, Status: Passed - CodeTree

--- 241010/초대장과 번호표/invitation-and-number-tag.py
+++ 241010/초대장과 번호표/invitation-and-number-tag.py
@@ -1,20 +1,35 @@
+from collections import deque
+
 n, g = map(int, input().split())
 
-invited = set({1})
-invitation = []
+invited = [False] * n
+
+groups = [set() for _ in range(g)]
+people_groups = [[] for _ in range(n)]
+
+queue = deque()
+ans = 0
 
 for i in range(g):
-    p = list(map(int, input().split()))[1:]
-    invitation.append(p)
+    nums = list(map(int, input().split()))[1:]
+    for num in nums:
+        num -= 1
+        groups[i].add(num)
+        people_groups[num].append(i)
 
-while True:
-    is_updated = False
-    for p in invitation:
-        p = set(p)
-        if len(p-invited) == 1:
-            invited.update(p-invited)
-            is_updated = True
-    if is_updated == False:
-        break
+queue.append(0)
+invited[0] = True
 
-print(len(invited))
+while queue:
+    x = queue.popleft()
+    ans += 1
+
+    for g_num in people_groups[x]:
+        groups[g_num].remove(x)
+        if len(groups[g_num]) == 1:
+            p_num = list(groups[g_num])[0]
+            if not invited[p_num]:
+                invited[p_num] = True
+                queue.append(p_num)
+
+print(ans)
