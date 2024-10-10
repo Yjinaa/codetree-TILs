9d625fa429b4a2f3a64fda189070d2445487553e
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-10-10 13:51:40 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 129ms, Memory: 31MB, Status: Passed - CodeTree

--- 241010/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
+++ 241010/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -7,10 +7,11 @@ max_val = ns[0]
 cur_sum = ns[0]
 
 for i in range(1,n):
+    if cur_sum < 0:
+        cur_sum = 0
     cur_sum += ns[i]
     if cur_sum > max_val:
         max_val = cur_sum
-    if cur_sum < 0:
-        cur_sum = 0
+
 
 print(max_val)
