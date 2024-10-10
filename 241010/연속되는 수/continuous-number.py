ef659c53b195a924b87876f0c154fcf156e2cbf0
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-10-10 17:55:46 +0900

    Title: 연속되는 수, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241010/연속되는 수/continuous-number.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+ns = [int(input()) for _ in range(n)]
+nums = set(ns)
+
+max_length = 0
+skip = 0
+for num in nums:
+    start_idx = 0
+    for end_idx in range(1,n):
+        if ns[0] == num:
+            start_idx = 1
+            continue
+        if ns[end_idx] == num:
+            skip += 1
+            continue
+        if ns[start_idx] != ns[end_idx]:
+            cur_length = end_idx - start_idx - skip
+            start_idx = end_idx
+            skip = 0
+            max_length = max(cur_length, max_length)
+        
+print(max_length)
