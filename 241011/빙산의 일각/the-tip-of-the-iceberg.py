8a85ff87d8c388599f5aeab593b0efc4c867c747
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2024-10-11 16:15:46 +0900

    Title: 빙산의 일각, Time: 5096ms, Memory: 30MB, Status: Time Limit Exceed - CodeTree

--- 241011/빙산의 일각/the-tip-of-the-iceberg.py
+++ 241011/빙산의 일각/the-tip-of-the-iceberg.py
@@ -9,13 +9,16 @@ for h in range(h-1,0,-1): # 잠기면 양수 안잠기면 음수
     after = [h-g for g in gs]
     prev_g = None
     cur_chunks = 0
-    for i in range(n):
-        if (prev_g == 0 or prev_g == None) and after[i] < 0:
+    pointer = 0
+    while pointer < n:
+        if (prev_g == 0 or prev_g == None) and after[pointer] < 0:
             cur_chunks += 1
-        if after[i] < 0:
+        if after[pointer] < 0:
             prev_g = 1
+            pointer += 1
         else:
             prev_g = 0
+            pointer += 1
     if cur_chunks > max_chunks:
         max_chunks = cur_chunks
         fin_h = h
