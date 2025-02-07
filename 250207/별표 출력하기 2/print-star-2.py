3d72322dd7b3db25f374d5b465cedc21ff878664
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-02-07 18:49:42 +0900

    Title: 별표 출력하기 2, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250207/별표 출력하기 2/print-star-2.py
+++ 250207/별표 출력하기 2/print-star-2.py
@@ -1,6 +1,14 @@
+# n = int(input())
+
+# for i in range(n):
+#     for j in range(n,i,-1): # 이렇게 할 필요가 없다 .j는 그냥 반복횟수를 담당하는 변수라서..
+#         print('*', end=" ")
+#     print()
+
+
 n = int(input())
 
 for i in range(n):
-    for j in range(n,i,-1):
-        print('*', end=" ")
+    for j in range(n-i): # 행이 늘어날 때마다 열 개수는 하나씩 줄어야하니까 n-i
+        print("*", end=" ")
     print()
