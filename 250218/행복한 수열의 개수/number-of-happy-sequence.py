3f804bfad94216e684fc42a80efa0a7dca715a4a
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-02-18 18:04:50 +0900

    Title: 행복한 수열의 개수, Time: 50ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250218/행복한 수열의 개수/number-of-happy-sequence.py
+++ 250218/행복한 수열의 개수/number-of-happy-sequence.py
@@ -4,18 +4,42 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 
 # Write your code here!
 
-def get_happies(grid):
+# def get_happies(grid):
+#     happies = 0
+#     for i in range(2):
+#         for row in range(n):
+#             nums = []
+#             for col in range(n):
+#                 nums.append(grid[row][col])
+#             cnt = Counter(nums)
+#             if m in cnt.values():
+#                 happies += 1
+#         grid = list(map(list, zip(*grid)))
+#     return happies
+
+
+# print(get_happies(grid))
+# 작업이 복잡할 땐 작은 작업 단위로 나눠보기
+def check_happy(nums, m):
+    cnt = 0
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i-1]:
+            cnt += 1
+        else:
+            cnt = 1
+    if cnt >= m:
+        return True
+    else:
+        return False
+
+
+def get_happies(grid,m):
     happies = 0
-    for i in range(2):
-        for row in range(n):
-            nums = []
-            for col in range(n):
-                nums.append(grid[row][col])
-            cnt = Counter(nums)
-            if m in cnt.values():
+    for _ in range(2):
+        for row in grid:
+            if check_happy(row,m):
                 happies += 1
         grid = list(map(list, zip(*grid)))
     return happies
 
-
-print(get_happies(grid))
+print(get_happies(grid,m))
