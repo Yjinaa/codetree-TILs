d70bd763ab5f46e39e96eeb49cacb77f0b95f7ee
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-02-28 21:44:29 +0900

    Title: 격자 위의 편안한 상태, Time: 81ms, Memory: 18MB, Status: Wrong Answer - CodeTree

--- 250228/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
+++ 250228/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -1,5 +1,5 @@
 n, m = map(int, input().split())
-grid = [[0 for _ in range(n+1)] for _ in range(n+1)]
+grid = [[0 for _ in range(n)] for _ in range(n)]
 
 # Please write your code here.
 
@@ -8,11 +8,12 @@ dy = [1, 0, -1, 0]
 
 for _ in range(m):
     r, c = map(int, input().split())
+    r, c = r-1, c-1
     grid[r][c] = 1
     colored = 0
     for i in range(4):
         nr, nc = r + dx[i], c + dy[i]
-        if 1 <= nr < n+1 and 1 <= nc < n+1 and grid[nr][nc]:
+        if 0 <= nr < n and 0 <= nc < n and grid[nr][nc]:
             colored += 1
     if colored >= 3:
         print(1)
