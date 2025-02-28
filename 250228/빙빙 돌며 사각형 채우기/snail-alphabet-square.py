8fef73eddfd2a5c6c582d87c94498de6302eb8bd
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-02-28 22:44:57 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250228/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
+++ 250228/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -10,9 +10,12 @@ dy = [1, 0, -1, 0]
 x, y = 0, 0
 d = 0
 
-for i in range(65, 65+m*n):
-    grid[x][y] = chr(i)
-
+char_num = 65
+for i in range(1,m*n+1):
+    if char_num > 90:
+        char_num = 65
+    grid[x][y] = chr(char_num)
+    char_num += 1
     nx, ny = x + dx[d], y + dy[d]
     if not (0 <= nx < n and 0 <= ny < m) or grid[nx][ny] != 0:
         d = (d+1)%4
