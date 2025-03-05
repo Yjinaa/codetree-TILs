0913dc4dcb677096441a9e9eea43570319d93ac8
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-03-05 21:36:58 +0900

    Title: 숫자들의 합 중 최대, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 250305/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
+++ 250305/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
@@ -19,3 +19,36 @@ for num in range(X, Y+1):
     ans = max(ans,s)
 
 print(ans)
+
+
+# =====================
+# 자리수가 유동적일 경우
+# =====================
+
+# def sum_of_digits(num):
+#     result = 0
+    
+#     # ========================================
+#     # 1의 자리부터 떼면서 num을 한자리씩 소거
+#     # ========================================
+#     while num > 0: 
+#         result += num % 10
+#         num /= 10
+
+#     # ========================================
+#     # 최대 범위부터 num을 한자리씩 소거
+#     # ========================================
+#     for i in range(1,10000,i*10):
+#         result += (num/i*10) % i
+
+#     result += (num / 10000) % 1000
+#     result += (num / 1000) % 100
+#     result += (num / 100) % 10
+#     result += (num / 10) % 1
+    
+#     return result
+
+# ans = 0
+# for num in range(X, Y+1):
+#     result = sum_of_digits(num)
+#     ans = max(ans, result)
