9c1c5596c29d81c9dca8c639d0c4166722552fcb
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-03-05 21:56:38 +0900

    Title: 데이터센터의 온도 조정 2, Time: 59ms, Memory: 17MB, Status: Passed - CodeTree

--- 250305/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
+++ 250305/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -21,3 +21,37 @@ for temp in range(start, end+1):
         workload += get_workload(ta,tb,temp)
     ans = max(ans, workload)
 print(ans)
+
+# ================
+# # 강의 답안
+# ================
+
+# # get_workload 함수는 똑같음
+
+# ans = 0
+# # 1에서 1000까지 모든 범위에 대해 탐색
+# for t in range(0,1001):
+#     total_output = 0
+#     # N개의 장비에 대해 각각 작업량 계산하여 누적
+
+#     # =======================
+#     # 1. 기존 접근방식
+#     # =======================
+
+#     for i in range(N):
+#         total_output += output(t, ranges[i][0], ranges[i][1])
+    
+#     # ========================
+#     # 2. 좀더 pythonic
+#     # ========================
+#     total_output = sum([
+#         output(t, T_a, T_b)
+#         for T_a, T_b in ranges
+#     ])
+
+    
+#     ans = max(ans, total_output)
+
+# print(ans)
+
+
