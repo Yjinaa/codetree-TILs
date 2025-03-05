9d06ebb426ac1db038956786e6e481d69b7778d1
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-03-05 19:30:54 +0900

    Title: 괄호 쌍 만들어주기 3, Time: 40ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250305/괄호 쌍 만들어주기 3/pair-parentheses-3.py
@@ -0,0 +1,14 @@
+A = list(input())
+
+# n이 10000이하면 n^3 안됨 100까진 가능
+# Please write your code here.
+
+cnt = 0
+
+for i in range(len(A)):
+    if A[i] == '(':
+        for j in range(i+1,len(A)): # i번째 문자부터 끝까지 비교
+            if A[j] == ')':
+                cnt += 1
+
+print(cnt)
