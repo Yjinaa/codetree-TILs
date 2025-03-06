4f366fa1cf52f67200708e38c1dca596f00c7ffa
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-03-06 19:39:38 +0900

    Title: 바구니 안의 사탕 2, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250306/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -0,0 +1,31 @@
+N, K = map(int, input().split())
+candy = []
+pos = []
+
+candies = sorted([tuple(map(int, input().split())) for _ in range(N)], key=lambda x:x[1])
+candy = [p[0] for p in candies]
+pos = [p[1] for p in candies]
+
+# ans = 0
+# for c in range(pos[0]+K, pos[-1]+1-K): # 모든 c의 경우의 수
+#     cnt = 0
+#     for i in range(c-K, c+K+1):
+#         for p in range(len(pos)):
+#             if c - K <= pos[p] <= c + K:
+#                 cnt += candy[p]
+#     ans = max(cnt, ans)
+
+# print(ans)
+
+# c는 안구해도 됨 걍 거리만 구하면 됨
+
+ans = 0
+for start in range(pos[0], pos[-1]-2*K+1): # 모든 c의 경우의 수
+    end = start + 2*K
+    cnt = 0
+    for p in range(len(pos)):
+        if start <= pos[p] <= end:
+            cnt += candy[p]
+    ans = max(cnt, ans)
+
+print(ans)
