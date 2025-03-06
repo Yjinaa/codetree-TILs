7d3a347c9ac9d0d7d6329d60ed68b79422cf4397
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-03-06 19:17:24 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250306/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 250306/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -1,12 +1,24 @@
-A = list(input())
+A = input()
 
 # Please write your code here.
 
-cnt = 0
-for i in range(len(A)-3):
-    if A[i] == '(' and A[i+1] == '(':
-        for j in range(i+2, len(A)-1):
-            if A[j] == ')' and A[j+1] == ')':
-                cnt += 1
+# cnt = 0
+# for i in range(len(A)-1):
+#     if A[i] == '(' and A[i+1] == '(':
+#         for j in range(i+2, len(A)-1):
+#             if A[j] == ')' and A[j+1] == ')':
+#                 cnt += 1
 
+# print(cnt)
+
+# ===================
+# 리스트 슬라이싱 사용
+# ===================
+
+cnt = 0
+for i in range(len(A)-1):
+    if A[i:i+2] == '((':
+        for j in range(i+2, len(A)-1):
+            if A[j:j+2] == '))':
+                cnt += 1
 print(cnt)
