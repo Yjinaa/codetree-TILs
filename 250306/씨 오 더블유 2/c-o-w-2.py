3df2b345c03c68892b57ce02f2a58b135798af73
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-03-06 19:20:25 +0900

    Title: 씨 오 더블유 2, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250306/씨 오 더블유 2/c-o-w-2.py
@@ -0,0 +1,15 @@
+n = int(input())
+s = input()
+
+# Please write your code here.
+
+cnt = 0
+for i in range(n):
+    if s[i] == 'C':
+        for j in range(i+1, n):
+            if s[j] == 'O':
+                for k in range(j+1,n):
+                    if s[k] == 'W':
+                        cnt += 1
+
+print(cnt)
