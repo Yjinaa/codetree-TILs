5f887ec72816355d98912a5c3a901ae8346709da
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-03-06 19:59:17 +0900

    Title: 바구니 안의 사탕 2, Time: 49ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250306/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 250306/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,10 +1,22 @@
-N, K = map(int, input().split())
+n,k = map(int, input().split())
 candy = []
 pos = []
 
-candies = sorted([tuple(map(int, input().split())) for _ in range(N)], key=lambda x:x[1])
+candies = sorted([tuple(map(int, input().split())) for _ in range(n)], key=lambda x:x[1])
 candy = [p[0] for p in candies]
 pos = [p[1] for p in candies]
+
+ans = 0
+for start in range(0, 200-(2*k)+1): # 모든 구간의 경우의 수
+    end = start + 2*k
+    cnt = 0
+    for p in range(len(pos)):
+        if start <= pos[p] <= end:
+            cnt += candy[p]
+    ans = max(cnt, ans)
+
+print(ans)
+
 
 # ans = 0
 # for c in range(pos[0]+K, pos[-1]+1-K): # 모든 c의 경우의 수
@@ -19,13 +31,14 @@ pos = [p[1] for p in candies]
 
 # c는 안구해도 됨 걍 거리만 구하면 됨
 
-ans = 0
-for start in range(pos[0], pos[-1]-2*K+1): # 모든 c의 경우의 수
-    end = start + 2*K
-    cnt = 0
-    for p in range(len(pos)):
-        if start <= pos[p] <= end:
-            cnt += candy[p]
-    ans = max(cnt, ans)
-
-print(ans)
+# ans = 0
+# print(pos[0],pos[-1])
+# for start in range(pos[0], pos[-1]-2*K+1): # 모든 c의 경우의 수
+#     end = start + 2*K
+#     cnt = 0
+#     print(len(pos))
+#     for p in range(len(pos)):
+#         if start <= pos[p] <= end:
+#             print(start, end)
+#             cnt += candy[p]
+#     ans = max(cnt, ans)
