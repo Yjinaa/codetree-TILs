00da3f7e78e28b705ee518dd3c0693659302e06a
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-03-07 19:21:15 +0900

    Title: 최고의 33위치, Time: 55ms, Memory: 16MB, Status: Passed - CodeTree

--- 250307/최고의 33위치/best-place-of-33.py
+++ 250307/최고의 33위치/best-place-of-33.py
@@ -5,11 +5,12 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 ans = 0
 
 for i in range(n):
-    cnt = 0
-    for row in range(i,i+3):
-        for col in range(i,i+3):
-            if 0 <= row < n and 0 <= col < n and grid[row][col] == 1:
-                cnt += 1
-    ans = max(cnt,ans)
+    for j in range(n):
+        cnt = 0
+        for row in range(i,i+3):
+            for col in range(j,j+3):
+                if 0 <= row < n and 0 <= col < n and grid[row][col] == 1:
+                    cnt += 1
+        ans = max(cnt,ans)
 
 print(ans)
