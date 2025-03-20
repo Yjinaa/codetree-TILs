36cf185384d02a07f83a571be9e2a38f0d3702e2
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-03-21 03:32:30 +0900

    Title: 1차원 젠가, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

--- 250321/1차원 젠가/jenga-1d.py
+++ 250321/1차원 젠가/jenga-1d.py
@@ -5,14 +5,14 @@ s2, e2 = map(int, input().split())
 
 # Please write your code here.
 
-blocks[-s1:-e1-1:-1] = [0]*(e1-s1+1)
+blocks[s1-1:e1] = [0]*(e1-s1+1)
 temp = []
 for i in range(len(blocks)):
     if blocks[i] != 0:
         temp.append(blocks[i])
 blocks = temp
 
-blocks[-s2:-e2-1:-1] = [0]*(e2-s2+1)
+blocks[s2-1:e2] = [0]*(e2-s2+1)
 temp = []
 for i in range(len(blocks)):
     if blocks[i] != 0:
