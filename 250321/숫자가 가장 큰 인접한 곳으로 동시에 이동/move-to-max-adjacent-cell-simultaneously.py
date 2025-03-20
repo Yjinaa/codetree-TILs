b2e3896e325e975895cd8ca66db3c54a230eaf59
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-03-21 05:04:44 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 71ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250321/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 250321/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -2,38 +2,45 @@ n, m, t = map(int, input().split())
 
 # Create n x n grid
 a = [list(map(int, input().split())) for _ in range(n)]
-loc = [[0 for _ in range(n)] for _ in range(n)]
 
 # Get m marble positions
 marbles = [tuple(map(int, input().split())) for _ in range(m)]
-r = [pos[0] for pos in marbles]
-c = [pos[1] for pos in marbles]
+marbles = [(r-1,c-1) for r,c in marbles]
 
 # Please write your code here.
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
-for k in range(t):
+def get_marbles(loc):
     temp = []
+    for i in range(n):
+        for j in range(n):
+            if loc[i][j] == 1:
+                temp.append((i,j))
+    return temp
+
+for k in range(t):
+    loc = [[0 for _ in range(n)] for _ in range(n)]
     for i in range(len(marbles)):
         r, c = marbles[i]
-        r, c = r-1, c-1
         cur = a[r][c]
+        moved = False
         for j in range(4):
             nr, nc = r + dx[j], c + dy[j]
             if 0 <= nr < n and 0 <= nc < n:
                 if a[nr][nc] > cur:
                     _r,_c = nr, nc
                     cur = a[nr][nc]
-        r,c = _r,_c
+                    moved = True
+        if moved == True:
+            r,c = _r,_c
         loc[r][c] += 1
-        if loc[r][c] > 1:
-            loc[r][c] = 0
-        else:
-            temp.append(((r,c)))
-    marbles = temp
+    marbles = get_marbles(loc)
 
 print(len(marbles))
+
+# 1. 최대값으로 갱신하는 로직 빼먹음 (_r, _c 사용하지 않고 r,c = nr,nc로 갱신하는 바람에 옮길 때마다 r,c가 갱신되어 기존 기준 좌표가 흔들리게 됨)
+# 2. 최대값이 더이상 없어 구슬이 움직이지 않을 경우 처리 안함 > 움직이지 않았는데 그냥 마지막 위치에 +1 해버리니까 2가 돼서 터짐 > moved = True/False 플래그 활용으로 움직였을 때만 새로운 위치, 안움직였을 경우 기존 r,c에 +1
 
         
 
