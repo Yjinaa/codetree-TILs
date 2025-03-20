63ab3116ea910d16d054e7474522087a607890b5
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-03-21 05:25:59 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 74ms, Memory: 17MB, Status: Wrong Answer - CodeTree

--- 250321/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 250321/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -6,6 +6,7 @@ a = [list(map(int, input().split())) for _ in range(n)]
 # Get m marble positions
 marbles = [tuple(map(int, input().split())) for _ in range(m)]
 marbles = [(r-1,c-1) for r,c in marbles]
+loc = [[0 for _ in range(n)] for _ in range(n)]
 
 # Please write your code here.
 dx = [-1,1,0,0]
@@ -17,6 +18,8 @@ def get_marbles(loc):
         for j in range(n):
             if loc[i][j] == 1:
                 temp.append((i,j))
+            elif loc[i][j] > 1:
+                loc[i][j] = 0
     return temp
 
 for k in range(t):
@@ -41,6 +44,6 @@ print(len(marbles))
 
 # 1. 최대값으로 갱신하는 로직 빼먹음 (_r, _c 사용하지 않고 r,c = nr,nc로 갱신하는 바람에 옮길 때마다 r,c가 갱신되어 기존 기준 좌표가 흔들리게 됨)
 # 2. 최대값이 더이상 없어 구슬이 움직이지 않을 경우 처리 안함 > 움직이지 않았는데 그냥 마지막 위치에 +1 해버리니까 2가 돼서 터짐 > moved = True/False 플래그 활용으로 움직였을 때만 새로운 위치, 안움직였을 경우 기존 r,c에 +1
-
-        
-
+# 3. loc 값을 매번 새로 갱신해서 이전 턴에서 구슬 있던 위치 + 다음 턴에서 구슬 겹칠 경우 생각 안함
+# 4. loc 하나에서 다 하려고 함. loc에서만 하면 이전 구슬위치 +1이 되어있는 상태에서 다음 위치까지 +1이 됨 기존 위치와 새 위치 구분 불가
+#    new_loc으로 기존위치와 새 위치 구분 필요
