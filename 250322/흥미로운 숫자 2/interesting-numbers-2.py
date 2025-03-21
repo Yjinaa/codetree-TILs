d1f3aed96beccfe30f94a00813faa1f477c55292
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-03-22 03:25:09 +0900

    Title: 흥미로운 숫자 2, Time: 341ms, Memory: 20MB, Status: Passed - CodeTree

--- 250322/흥미로운 숫자 2/interesting-numbers-2.py
+++ 250322/흥미로운 숫자 2/interesting-numbers-2.py
@@ -1,10 +1,13 @@
+from collections import Counter
 X, Y = map(int, input().split())
 
 # Please write your code here.
 
 cnt = 0
 for i in range(X, Y+1):
-    if len(set(map(int, list(str(i))))) == 2:
+    num = list(map(int, list(str(i))))
+    counts = Counter(num)
+    if len(counts) == 2 and 1 in counts.values():
         cnt += 1
 
 print(cnt)
