52c43a39f0e0775e4af3a8aec2b97746841be4e8
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-03-22 03:15:22 +0900

    Title: 이상한 폭탄 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250322/이상한 폭탄 2/strange-bomb-2.py
+++ 250322/이상한 폭탄 2/strange-bomb-2.py
@@ -6,10 +6,9 @@ num = [int(input()) for _ in range(N)]
 max_exploded = -100
 is_exploded = False
 for i in range(0,N-K+1): # 시작점
-    ranges = num[i:i+K+1]
-    if len(ranges) != len(set(ranges)):
+    if num[i] in num[i+1:i+K+1]:
+        exploded = num[i]
         is_exploded = True
-        exploded = max(num[i:i+K+1])
         max_exploded = max(max_exploded, exploded)
 
 if is_exploded == False:
