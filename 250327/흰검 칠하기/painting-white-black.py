a58d94e694657b8053440faece1de320401c566b
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-03-27 03:39:09 +0900

    Title: 흰검 칠하기, Time: 91ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 250327/흰검 칠하기/painting-white-black.py
+++ 250327/흰검 칠하기/painting-white-black.py
@@ -5,9 +5,9 @@ n = int(input())
 # 흰색, 검은색, 현재 색 리스트를 따로 만들어 관리
 # 현재 색에서는 모두 -1로 초기화, 흰색이면 0 검은색이면 1로 칠하기
 
-w = [0] * 2010
-b = [0] * 2010
-c = [-1] * 2010
+w = [0] * 20010
+b = [0] * 20010
+c = [-1] * 20010
 
 segments = []
 
@@ -17,25 +17,25 @@ for _ in range(n):
     x = int(x)
     
     if direc == 'L':
-        left = cur_loc - x
+        left = cur_loc - x + 1
         right = cur_loc 
-        cur_loc -= x
+        cur_loc = cur_loc - x + 1
         color = 'w'
     else:
         left = cur_loc 
-        right = cur_loc + x
-        cur_loc += x
+        right = cur_loc + x - 1
+        cur_loc = cur_loc + x - 1
         color = 'b'
     segments.append([left,right,color])
 
 for seg in segments:
     l,r,color = seg[0] + 1000, seg[1] + 1000, seg[2]
     if color == 'w':
-        for i in range(l,r):
+        for i in range(l,r+1):
             w[i] += 1
             c[i] = 0
     else:
-        for i in range(l,r):
+        for i in range(l,r+1):
             b[i] += 1
             c[i] = 1
 w_cnt = 0
@@ -51,3 +51,32 @@ for j in range(len(w)):
             w_cnt += 1
 
 print(w_cnt, b_cnt, g_cnt)
+
+# cur_loc = 1000
+# for _ in range(n):
+#     x, direc = input().split()
+#     x = int(x)
+#     if direc == 'L':
+#         for i in range(cur_loc-1, cur_loc-x-1, -1):
+#             w[i] += 1
+#             c[i] = 0
+#         cur_loc -= x
+#     else:
+#         for i in range(cur_loc, cur_loc+x):
+#             b[i] += 1
+#             c[i] = 1
+#         cur_loc += x
+
+# w_cnt = 0
+# b_cnt = 0
+# g_cnt = 0
+# for j in range(len(w)):
+#     if w[j] >= 2 and b[j] >= 2:
+#         g_cnt += 1
+#     else:
+#         if c[j] == 1:
+#             b_cnt += 1
+#         elif c[j] == 0:
+#             w_cnt += 1
+
+# print(w_cnt, b_cnt, g_cnt)
