8dc23d9fc14bc223ad1713a227b3f3a3889dd523
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-03-27 05:00:50 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 143ms, Memory: 49MB, Status: Passed - Codetree

--- 250327/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 250327/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -14,13 +14,18 @@ min_x = 2010
 min_y = 2010
 max_x = 0
 max_y = 0
+cnt = 0
 for x in range(0,2010):
     for y in range(0,2010):
         if grid[x][y] == 1:
+            cnt += 1
             min_x = min(min_x, x)
             min_y = min(min_y, y)
             max_x = max(max_x, x)
             max_y = max(max_y, y)
 
-print((max_x-min_x+1)*(max_y-min_y+1))
+if cnt == 0:
+    print(0)
+else:
+    print((max_x-min_x+1)*(max_y-min_y+1))
 
