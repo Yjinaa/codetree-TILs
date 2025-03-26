92a020733a6d74b9636bc56f2d3adb25565ab709
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-03-26 18:50:19 +0900

    Title: DateTime to DateTime, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250326/DateTime to DateTime/datetime-to-datetime.py
+++ 250326/DateTime to DateTime/datetime-to-datetime.py
@@ -2,24 +2,32 @@ a, b, c = map(int, input().split())
 
 # Please write your code here.
 
-months = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+# months = [0,31,28,31,30,31,30,31,31,30,31,30,31]
 
-month = 11
-day = 11
-hour = 11
-mins = 11
-elapsed_time = 0
+# month = 11
+# day = 11
+# hour = 11
+# mins = 11
+# elapsed_time = 0
 
-while True:
-    if day == a and hour == b and mins == c:
-        print(elapsed_time)
-        break
-    mins += 1
-    elapsed_time += 1
-    if mins == 60:
-        hour += 1
-        mins = 0
-        if hour == 24:
-            day += 1
-            hour = 0
+# while True:
+#     if day == a and hour == b and mins == c:
+#         print(elapsed_time)
+#         break
+#     mins += 1
+#     elapsed_time += 1
+#     if mins == 60:
+#         hour += 1
+#         mins = 0
+#         if hour == 24:
+#             day += 1
+#             hour = 0
+
+diff = (a*24*60 + b*60 + c) - (11*24*60 + 11*60 + 11)
+
+if diff < 0:
+    print(-1)
+else:
+    print(diff)
+
 
