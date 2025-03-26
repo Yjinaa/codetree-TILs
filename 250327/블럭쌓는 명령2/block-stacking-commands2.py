9cc1a406c3425d2ef7a77c46217881e420603885
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-03-27 02:06:59 +0900

    Title: 블럭쌓는 명령2, Time: 40ms, Memory: 16MB, Status: Passed - Codetree

--- 250327/블럭쌓는 명령2/block-stacking-commands2.py
+++ 250327/블럭쌓는 명령2/block-stacking-commands2.py
@@ -2,7 +2,7 @@ n, k = map(int, input().split())
 
 # Please write your code here.
 
-ans = [0 for _ in range(n)]
+ans = [0 for _ in range(n+1)]
 
 for i in range(k):
     a, b = map(int, input().split())
