b015756224695ebea2f1329bde9b7e406cdb074a
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-03-29 03:00:49 +0900

    Title: 연속되는 수 2, Time: 66ms, Memory: 16MB, Status: Passed - Codetree

--- 250329/연속되는 수 2/continuous-number2.py
+++ 250329/연속되는 수 2/continuous-number2.py
@@ -5,10 +5,12 @@ arr = [int(input()) for _ in range(n)]
 
 cur = 1
 longest = 0
+
 for i in range(n):
     if i == 0 or arr[i] != arr[i-1]:
         longest = max(longest, cur)
         cur = 0
     cur += 1
 
+longest = max(longest, cur)
 print(longest)
