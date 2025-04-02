a00f538751e34c1d181af5fce1f0a9faefa2e882
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-04-02 19:10:10 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250402/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
+++ 250402/빙빙 돌며 숫자 사각형 채우기 2/snail-number-square-2.py
@@ -9,11 +9,14 @@ dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
 for i in range(1, n*m):
-    nx, ny = x + dx[d], y + dy[d]
-    if not (0 <= nx < n and 0 <= ny < m) or grid[nx][ny] != 0:
-        d = (d+3)%4
-    x, y = x + dx[d], y + dy[d]
-    grid[x][y] = i+1
+    while True:
+        nx, ny = x + dx[d], y + dy[d]
+        if not (0 <= nx < n and 0 <= ny < m) or grid[nx][ny] != 0:
+            d = (d+3)%4
+        else:
+            x, y = x + dx[d], y + dy[d]
+            grid[x][y] = i+1
+            break
 
 for row in grid:
     print(*row)
