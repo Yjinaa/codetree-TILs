45fda9592ff21573eb70102bac49082b6167c3e0
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-04-04 02:17:15 +0900

    Title: 인접하지 않은 2개의 수, Time: 51ms, Memory: 17MB, Status: Passed - Codetree

--- 250404/인접하지 않은 2개의 수/two-non-adjacent-numbers.py
+++ 250404/인접하지 않은 2개의 수/two-non-adjacent-numbers.py
@@ -6,7 +6,7 @@ numbers = list(map(int, input().split()))
 max_sum = 0
 cur_sum = 0
 for i in range(n-2):
-    for j in range(i+1, n):
+    for j in range(i+2, n):
         cur_sum = max(cur_sum, numbers[i]+numbers[j])
     max_sum = max(cur_sum, max_sum)
 
