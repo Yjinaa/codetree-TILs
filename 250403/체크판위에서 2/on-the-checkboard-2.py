26bd9632a8fba837e6fab05081980aaa158b0665
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-04-03 19:31:20 +0900

    Title: 체크판위에서 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250403/체크판위에서 2/on-the-checkboard-2.py
+++ 250403/체크판위에서 2/on-the-checkboard-2.py
@@ -2,20 +2,33 @@ R, C = map(int, input().split())
 grid = [list(input().split()) for _ in range(R)]
 
 # Please write your code here.
-queue = []
 
-row, col, jumped = 0, 0, 0
-queue.append((row,col, jumped))
+## bfs 풀이
+# queue = []
+
+# row, col, jumped = 0, 0, 0
+# queue.append((row,col, jumped))
+# cnt = 0
+
+# while queue:
+#     row, col, jumped = queue.pop()
+#     for i in range(row+1, R):
+#         for j in range(col+1, C):
+#             if i == R-1 and j == C-1:
+#                 if jumped == 2 and grid[row][col] != grid[i][j]:
+#                     cnt += 1
+#                 continue
+#             if grid[row][col] != grid[i][j]:
+#                 queue.append((i,j,jumped+1))
+# print(cnt)
+
+## 완탐 풀이 - 점프 2번이라고 해서 가능
 cnt = 0
 
-while queue:
-    row, col, jumped = queue.pop()
-    for i in range(row+1, R):
-        for j in range(col+1, C):
-            if i == R-1 and j == C-1:
-                if jumped == 2 and grid[row][col] != grid[i][j]:
-                    cnt += 1
-                continue
-            if grid[row][col] != grid[i][j]:
-                queue.append((i,j,jumped+1))
+for row in range(1, R):
+    for col in range(1, C):
+        for i in range(row+1, R-1):
+            for j in range(col+1, C-1):
+               if grid[0][0] != grid[row][col] and grid[row][col] != grid[i][j] and grid[i][j] != grid[R-1][C-1]:
+                cnt += 1 
 print(cnt)
