8b674ece1f0410205df3cb34357b44598e7debee
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-04-04 01:48:52 +0900

    Title: 이상한 진수 2, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250404/이상한 진수 2/awkward-digits-2.py
+++ 250404/이상한 진수 2/awkward-digits-2.py
@@ -1,19 +1,34 @@
-a = input()
+# a = input()
 
 # Please write your code here.
 
-max_n = 0
-for i in range(len(a)):
-    cur_char = ''
-    for j in range(len(a)):
-        if i == j:
-            cur_char += str(1-int(a[j]))
-            continue
-        cur_char += a[j]
+# max_n = 0
+# for i in range(len(a)):
+#     cur_char = ''
+#     for j in range(len(a)):
+#         if i == j:
+#             cur_char += str(1-int(a[j]))
+#             continue
+#         cur_char += a[j]
     
+#     n = 0
+#     for k in range(len(cur_char)):
+#         n = n * 2 + int(cur_char[k])
+#     max_n = max(max_n, n)
+
+# print(max_n)
+
+a = list(map(int, input()))
+max_n = 0
+
+for i in range(len(a)):
+    a[i] = 1-a[i]
+
     n = 0
-    for k in range(len(cur_char)):
-        n = n * 2 + int(cur_char[k])
-    max_n = max(max_n, n)
+    for j in range(len(a)):
+        n = n*2 + a[j]
+    max_n = max(n, max_n)
+
+    a[i] = 1-a[i]
 
 print(max_n)
