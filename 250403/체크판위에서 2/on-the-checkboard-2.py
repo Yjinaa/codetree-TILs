c85de2839e8aa0ee5f03c287a870abb4010dc3f3
Yjinaa <71372857+Yjinaa@users.noreply.github.com>
2025-04-03 19:27:26 +0900

    Title: 체크판위에서 2, Time: 71ms, Memory: 18MB, Status: Passed - Codetree

--- 250403/체크판위에서 2/on-the-checkboard-2.py
+++ 250403/체크판위에서 2/on-the-checkboard-2.py
@@ -12,8 +12,9 @@ while queue:
     row, col, jumped = queue.pop()
     for i in range(row+1, R):
         for j in range(col+1, C):
-            if i == R-1 and j == C-1 and jumped == 2:
-                cnt += 1
+            if i == R-1 and j == C-1:
+                if jumped == 2 and grid[row][col] != grid[i][j]:
+                    cnt += 1
                 continue
             if grid[row][col] != grid[i][j]:
                 queue.append((i,j,jumped+1))
